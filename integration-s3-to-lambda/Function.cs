418f8a39d879a7280ae3f5fbfcdfa8475cfb7458
James Eastham <jamesuk@amazon.com>
2022-08-12 13:24:35 +0100

    Add .NET S3 example

---
+++ integration-s3-to-lambda/Function.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+using Amazon.S3;
+using System;
+using Amazon.Lambda.S3Events;
+using System.Web;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace S3Integration
+{
+    public class Function
+    {
+        private static AmazonS3Client _s3Client;
+        public Function() : this(null)
+        {
+        }
+
+        internal Function(AmazonS3Client s3Client)
+        {
+            _s3Client = s3Client ?? new AmazonS3Client();
+        }
+
+        public async Task<string> Handler(S3Event evt, ILambdaContext context)
+        {
+            try
+            {
+                if (evt.Records.Count <= 0)
+                {
+                    context.Logger.LogLine("Empty S3 Event received");
+                    return string.Empty;
+                }
+
+                var bucket = evt.Records[0].S3.Bucket.Name;
+                var key = HttpUtility.UrlDecode(evt.Records[0].S3.Object.Key);
+
+                context.Logger.LogLine($"Request is for {bucket} and {key}");
+
+                var objectResult = await _s3Client.GetObjectAsync(bucket, key);
+
+                context.Logger.LogLine($"Returning {objectResult.Key}");
+
+                return objectResult.Key;
+            }
+            catch (Exception e)
+            {
+                context.Logger.LogLine($"Error processing request - {e.Message}");
+
+                return string.Empty;
+            }
+        }
+    }
+}
