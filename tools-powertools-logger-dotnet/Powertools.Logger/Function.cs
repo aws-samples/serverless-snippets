724876c1081b1c62e2b532dfb7c9e0d03633122a
James Eastham <jamesuk@amazon.com>
2022-08-12 12:50:01 +0100

    Add PowerTools Logger for .NET

---
+++ tools-powertools-logger-dotnet/Powertools.Logger/Function.cs
@@ -0,0 +1,35 @@
+﻿using Amazon.Lambda.Core;
+using AWS.Lambda.Powertools.Logging;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace Powertools.Logging
+{
+    public class Function
+    {
+        // Powertools Logger requires an environment variables against your function
+        // POWERTOOLS_SERVICE_NAME
+        [Logging(LogEvent = true)]
+        public void Handler(object request, ILambdaContext context)
+        {
+            try
+            {
+                var requestContextRequestId = Guid.NewGuid().ToString();
+
+                var lookupInfo = new Dictionary<string, object>()
+                {
+                    {"LookupInfo", new Dictionary<string, object>{{ "LookupId", requestContextRequestId }}}
+                };
+
+                // Append custom keys to the structured logs
+                Logger.AppendKeys(lookupInfo);
+
+                Logger.LogInformation("Getting ip address from external service");
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, "Failure processing request");
+            }
+        }
+    }
+}
---
+++ tools-powertools-logger-dotnet/README.md
@@ -0,0 +1,24 @@
+# Powertools Logger
+
+This snippet is an example of using Lambda Powertools for .NET to create structured application logs in Amazon CloudWatch.
+
+Learn more about this snippet at Serverless Land Snippets: https://serverlessland.com/snippets/tools-powertools-logger-dotnet
+
+Important: this application could use various AWS services and there are costs associated with these services after the Free Tier usage - please see the [AWS Pricing page](https://aws.amazon.com/pricing/) for details. You are responsible for any AWS costs incurred. No warranty is implied in this example.
+
+## How it works
+
+Uses Lambda Powertools for .NET to create structured logs from your application.
+
+[Follow the instructions on the snippet page](https://serverlessland.com/snippets/tools-powertools-logger-dotnet) to run in your AWS account.
+
+
+## Cleanup
+
+No clean up required.
+
+---
+
+Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+SPDX-License-Identifier: MIT-0
