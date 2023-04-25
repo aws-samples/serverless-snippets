83b7769e4e68a0edcee5539b6a81d02464eeba8f
Victor Feinman <feinmv@amazon.com>
2023-04-25 17:24:30 -0400

    Adding SQS handle & partial batch response

---
+++ lambda-function-sqs-report-batch-item-failures/example.java
@@ -0,0 +1,26 @@
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.amazonaws.services.lambda.runtime.events.SQSBatchResponse;
+ 
+import java.util.ArrayList;
+import java.util.List;
+ 
+public class ProcessSQSMessageBatch implements RequestHandler<SQSEvent, SQSBatchResponse> {
+    @Override
+    public SQSBatchResponse handleRequest(SQSEvent sqsEvent, Context context) {
+ 
+         List<SQSBatchResponse.BatchItemFailure> batchItemFailures = new ArrayList<SQSBatchResponse.BatchItemFailure>();
+         String messageId = "";
+         for (SQSEvent.SQSMessage message : sqsEvent.getRecords()) {
+             try {
+                 //process your message
+                 messageId = message.getMessageId();
+             } catch (Exception e) {
+                 //Add failed message identifier to the batchItemFailures list
+                 batchItemFailures.add(new SQSBatchResponse.BatchItemFailure(messageId));
+             }
+         }
+         return new SQSBatchResponse(batchItemFailures);
+     }
+}
