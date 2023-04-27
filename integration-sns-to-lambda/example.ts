9e9fbb2064077fe2240536430ce6f1801e5c825a
Mohammed Tomeh <tomemoha@amazon.com>
2023-04-26 22:56:53 -0500

    adding lambda handler for sns event in typescript

---
+++ integration-sns-to-lambda/example.ts
@@ -0,0 +1,12 @@
+import { SNSEvent, Context, SNSHandler } from 'aws-lambda';
+
+ export const functionHandler : SNSHandler = async (event: SNSEvent, context: Context) : Promise<void | undefined> => {
+        try {
+            const message : string = JSON.stringify(event.Records[0].Sns.Message);
+            console.log(message);
+        } catch (err) {
+            const errorMessage : string = `An error occurred - ${err}`;
+            console.error(errorMessage);
+            throw new Error(errorMessage);
+        }
+ };
