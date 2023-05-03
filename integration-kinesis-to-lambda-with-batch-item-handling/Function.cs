173fa22a053f616596d726e80d229663512e3aff
Mohammed Tomeh <tomemoha@amazon.com>
2023-05-02 19:00:53 -0500

    added lambda serilizer attribute

--- integration-kinesis-to-lambda-with-batch-item-handling/Function.cs
+++ integration-kinesis-to-lambda-with-batch-item-handling/Function.cs
@@ -4,7 +4,7 @@ using Amazon.Lambda.Core;
 using Amazon.Lambda.KinesisEvents;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
-//[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace KinesisIntegration;
 
