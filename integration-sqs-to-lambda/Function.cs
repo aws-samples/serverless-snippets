c94f4bea5c710384ab59589290ce66201ad91f00
Mohammed Tomeh <tomemoha@amazon.com>
2023-05-27 13:02:55 -0500

    adding node sample

--- integration-sqs-to-lambda/Function.cs
+++ integration-sqs-to-lambda/Function.cs
@@ -7,40 +7,21 @@ using Amazon.Lambda.SQSEvents;
 
 namespace SqsIntegrationSampleCode
 {
-    public class Function
+    public async Task<string> FunctionHandler(SQSEvent evnt, ILambdaContext context)
     {
-        /// <summary>
-        /// Default constructor. This constructor is used by Lambda to construct the instance. When invoked in a Lambda environment
-        /// the AWS credentials will come from the IAM role associated with the function and the AWS region will be set to the
-        /// region the Lambda function is executed in.
-        /// </summary>
-        public Function()
+        foreach (var message in evnt.Records)
         {
-
+            await ProcessMessageAsync(message, context);
         }
 
-        /// <summary>
-        /// This method is called for every Lambda invocation. This method takes in an SQS event object and can be used 
-        /// to respond to SQS messages.
-        /// </summary>
-        /// <param name="evnt"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public async Task<string> FunctionHandler(SQSEvent evnt, ILambdaContext context)
-        {
-            foreach (var message in evnt.Records)
-            {
-                await ProcessMessageAsync(message, context);
-            }
-            
-            return "done";
-        }
-
-        private async Task ProcessMessageAsync(SQSEvent.SQSMessage message, ILambdaContext context)
-        {
-            context.Logger.LogLine($"Processed message {message.Body}");
-            // TODO: Do interesting work based on the new message
-            await Task.CompletedTask;
-        }
+        return "done";
     }
+
+    private async Task ProcessMessageAsync(SQSEvent.SQSMessage message, ILambdaContext context)
+    {
+        context.Logger.LogLine($"Processed message {message.Body}");
+        // TODO: Do interesting work based on the new message
+        await Task.CompletedTask;
+    }
+}
 }
--- integration-sqs-to-lambda/example.js
+++ integration-sqs-to-lambda/example.js
@@ -1,16 +1,11 @@
-import { SQSEvent, Context, SQSHandler, SQSRecord } from "aws-lambda";
-
-export const functionHandler: SQSHandler = async (
-  event: SQSEvent,
-  context: Context
-): Promise<string> => {
+exports.handler = async (event, context) => {
   for (const message of event.Records) {
     await processMessageAsync(message);
   }
   return "done";
 };
 
-async function processMessageAsync(message: SQSRecord): Promise<any> {
+async function processMessageAsync(message) {
   console.log(`Processed message ${message.body}`);
   // TODO: Do interesting work based on the new message
   await Promise.resolve(1); //Placeholder for actual async work
