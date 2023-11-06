1405b8b364002e01fb6e33a7fbfe314d0e666be3
ssnskar <ssnskar@amazon.com>
2023-11-06 11:37:11 +0530

    Function.py
    Added the python code snippet of Tutorial: Using Lambda with Amazon SQS as per : https://app.asana.com/0/1203681999313912/1205789652274515

---
+++ integration-sqs-to-lambda/Function.py
@@ -0,0 +1,13 @@
+def process_message(message):
+    try:
+        print(f"Processed message {message['body']}")
+        # TODO: Do interesting work based on the new message
+    except Exception as err:
+        print("An error occurred")
+        raise err
+
+def lambda_handler(event, context):
+    
+    for message in event['Records']:
+        process_message(message)
+    print("done")
