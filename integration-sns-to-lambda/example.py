155b12e1a9b20facf15ca28f43b670221101ed1b
Himanshu Goyal <higoyal@amazon.com>
2023-11-06 11:37:25 +0530

    Create example.py

---
+++ integration-sns-to-lambda/example.py
@@ -0,0 +1,14 @@
+def lambda_handler(event, context):
+    for record in event['Records']:
+        process_message(record)
+    print("done")
+
+def process_message(record):
+    try:
+        message = record['Sns']['Message']
+        print(f"Processed message {message}")
+        # TODO; Process your record here
+        
+    except Exception as e:
+        print("An error occurred")
+        raise e
