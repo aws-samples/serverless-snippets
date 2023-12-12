8bffa42debdf90a746a16d2bdbb00f4db16fc6e6
Siarhei Kazhura <kazhura@amazon.com>
2023-12-12 13:45:55 -0800

    Consuming Kinesis event with Lambda using Rust without batch item handling.

---
+++ integration-kinesis-to-lambda/main.rs
@@ -0,0 +1,49 @@
+use aws_lambda_events::event::kinesis::KinesisEvent;
+use lambda_runtime::{run, service_fn, Error, LambdaEvent};
+
+static UNDEFINED: &str = "undefined";
+
+async fn function_handler(event: LambdaEvent<KinesisEvent>) -> Result<(), Error> {
+    
+    if event.payload.records.is_empty() {
+        tracing::info!("No records found. Exiting.");
+        return Ok(());
+    }
+
+    event.payload.records.iter().for_each(|record| {
+
+        tracing::info!("EventId: {}", record.event_id.as_deref().unwrap_or(UNDEFINED));
+
+        let record_data = std::str::from_utf8(&record.kinesis.data);
+
+        match record_data {
+            Ok(data) => {
+                // log the record data
+                tracing::info!("Data: {}", data);
+            }
+            Err(e) => {
+                tracing::error!("Error: {}", e);
+            }
+        }
+    });
+
+    tracing::info!(
+        "Successfully processed {} records",
+        event.payload.records.len()
+    );
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        // disable printing the name of the module in every log line.
+        .with_target(false)
+        // disabling time is handy because CloudWatch will add the ingestion time.
+        .without_time()
+        .init();
+
+    run(service_fn(function_handler)).await
+}
