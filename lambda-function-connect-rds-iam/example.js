56d2438ad4829218250aa4efb52d13aeadc1570a
Jon Loinaz <jloinaz@amazon.es>
2024-01-05 13:54:34 +0100

    Refactor: - Removed unnecessary declaration of username in RDSigner - Now token for auth is passed directly, no need for authPlugin - Added missing semicolon

--- lambda-function-connect-rds-iam/example.js
+++ lambda-function-connect-rds-iam/example.js
@@ -13,55 +13,43 @@ async function createAuthToken() {
     port: process.env.Port,
     username: process.env.DBUserName,
     region: process.env.AWS_REGION,
-  
+
   }
 
   // Create RDS Signer object
   const signer = new Signer(dbinfo);
-  
+
   // Request authorization token from RDS, specifying the username
-  const token = await signer.getAuthToken(
-    {
-      username: process.env.DBUserName,
-    }
-  );
+  const token = await signer.getAuthToken();
   return token;
 }
 
 async function dbOps() {
 
-  try {
-      // Obtain auth token
-      const token = await createAuthToken();
-      // Define connection configuration
-      let connectionConfig = {
-        host: process.env.ProxyHostName,
-        user: process.env.DBUserName,
-        //password: token,
-        database: process.env.DBName,
-        ssl: 'Amazon RDS',
-        authPlugins: {
-          mysql_clear_password: () => () => Buffer.from(token)
-        },
-      }
-      // Create the connection to the DB
-      const conn = await mysql.createConnection(connectionConfig);
-      // Obtain the result of the query
-      const [res,] = await conn.execute('select ?+? as sum', [2, 2]);
-      return res
-      
-  } catch (error) {
-      console.error(error)
-      return error
+  // Obtain auth token
+  const token = await createAuthToken();
+  // Define connection configuration
+  let connectionConfig = {
+    host: process.env.ProxyHostName,
+    user: process.env.DBUserName,
+    password: token,
+    database: process.env.DBName,
+    ssl: 'Amazon RDS'
   }
+  // Create the connection to the DB
+  const conn = await mysql.createConnection(connectionConfig);
+  // Obtain the result of the query
+  const [res,] = await conn.execute('select ?+? as sum', [3, 2]);
+  return res;
+
 }
 
-export const handler = async(event) => {
-      // Execute database flow
-      const result = await dbOps();
-      // Return result
-      return {
-        statusCode: 200,
-        body: JSON.stringify("The sum is: "+result[0].sum) 
-      }
+export const handler = async (event) => {
+  // Execute database flow
+  const result = await dbOps();
+  // Return result
+  return {
+    statusCode: 200,
+    body: JSON.stringify("The selected sum is: " + result[0].sum)
+  }
 };
