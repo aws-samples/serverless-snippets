737ff90f822a88003f3b732455fdd251becf5e92
Jan Richter <ricjanac@amazon.com>
2024-01-08 07:45:13 +1300

    feat: example for php
    Using Bref as it provides much of the boilerplate needed for PHP lambdas.

---
+++ integration-kinesis-to-lambda/example.php
@@ -0,0 +1,44 @@
+<?php
+
+# using bref/bref and bref/logger for simplicity
+
+use Bref\Context\Context;
+use Bref\Event\Kinesis\KinesisEvent;
+use Bref\Event\Kinesis\KinesisHandler;
+use Bref\Logger\StderrLogger;
+
+require __DIR__ . '/vendor/autoload.php';
+
+class Handler extends KinesisHandler
+{
+    private StderrLogger $logger;
+    public function __construct(StderrLogger $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * @throws JsonException
+     * @throws \Bref\Event\InvalidLambdaEvent
+     */
+    public function handleKinesis(KinesisEvent $event, Context $context): void
+    {
+        $this->logger->info("Processing records");
+        $records = $event->getRecords();
+        foreach ($records as $record) {
+            try {
+                $data = $record->getData();
+                $this->logger->info(json_encode($data));
+                // TODO: Do interesting work based on the new data
+            } catch (Exception $e) {
+                $this->logger->error($e->getMessage());
+                throw $e;
+            }
+        }
+        $totalRecords = count($records);
+        $this->logger->info("Successfully processed $totalRecords records");
+    }
+}
+
+$logger = new StderrLogger();
+return new Handler($logger);
