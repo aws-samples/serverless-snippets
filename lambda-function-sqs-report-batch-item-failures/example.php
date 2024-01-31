c9b81e56c364ade46e6291ae25537fbc226fcb7a
skamatka <skamatka@amazon.com>
2024-01-30 20:43:35 -0500

    updated the code

--- lambda-function-sqs-report-batch-item-failures/example.php
+++ lambda-function-sqs-report-batch-item-failures/example.php
@@ -1,29 +1,59 @@
-//Make sure you have Bref installed and the necessary dependencies. Run the following commands in your project directory:
-//composer require bref/bref
-//composer install
-
-
 <?php
 
 use Bref\Context\Context;
-use Bref\Event\SqsEvent;
+use Bref\Event\Sqs\SqsEvent;
+use Bref\Event\Handler as StdHandler;
+use Bref\Logger\StderrLogger;
 
+require __DIR__ . '/vendor/autoload.php';
 
-function lambda_handler(SQSEvent $event, Context $context) {
-    if ($event) {
-        $batch_item_failures = [];
-        $sqs_batch_response = [];
-     
-        foreach ($event->getRecords() as $record) {
+class Handler implements StdHandler
+{
+    private StderrLogger $logger;
+    public function __construct(StderrLogger $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * @throws JsonException
+     * @throws \Bref\Event\InvalidLambdaEvent
+     */
+    public function handle(mixed $event, Context $context): array
+    {
+        $sqsEvent = new SqsEvent($event);
+        $this->logger->info("Processing SQS records");
+        $records = $sqsEvent->getRecords();
+
+        $failedRecords = [];
+        foreach ($records as $record) {
             try {
-                // process message
+                // Assuming the SQS message is in JSON format
+                $message = json_decode($record->getBody(), true);
+                $this->logger->info(json_encode($message));
+                // TODO: Implement your custom processing logic here
             } catch (Exception $e) {
-                $batch_item_failures[] = ["itemIdentifier" => $record['messageId']];
+                $this->logger->error($e->getMessage());
+                // failed processing the record
+                $failedRecords[] = $record->getMessageId();
             }
         }
-        
-        $sqs_batch_response["batchItemFailures"] = $batch_item_failures;
-        return $sqs_batch_response;
+        $totalRecords = count($records);
+        $this->logger->info("Successfully processed $totalRecords SQS records");
+
+        // Format failures for the response
+        $failures = array_map(
+            fn(string $messageId) => ['itemIdentifier' => $messageId],
+            $failedRecords
+        );
+
+        return [
+            'batchItemFailures' => $failures
+        ];
     }
 }
+
+$logger = new StderrLogger();
+return new Handler($logger);
+
 ?>
