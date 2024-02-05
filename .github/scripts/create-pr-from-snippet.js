b17bd744481fe7b43915ac3c6427f8826abd358d
Boyne <dboyne@3c06304f253a.ant.amazon.com>
2024-02-05 11:46:57 +0000

    adding new template

--- .github/scripts/create-pr-from-snippet.js
+++ .github/scripts/create-pr-from-snippet.js
@@ -1,39 +1,54 @@
 module.exports = async ({ github, context, core }) => {
-
   console.log('Trigger', process.env);
 
-//   const prNumber = process.env.PR_NUMBER;
+  const issueNumber = process.env.ISSUE_NUMBER;
 
-//   if (prNumber === '') {
-//     core.setFailed(`No PR number was passed. Aborting`);
-//   }
+  if (!issueNumber) {
+    core.setFailed(`No issue number was passed. Aborting`);
+  }
 
-//   try {
-//     const {
-//       data: { head, base, user, ...rest },
-//     } = await github.rest.pulls.get({
-//       owner: context.repo.owner,
-//       repo: context.repo.repo,
-//       pull_number: prNumber,
-//     });
+  const { response } = await github.issues.get({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: issueNumber,
+  });
 
-//     const { data: fileData } = await github.rest.pulls.listFiles({
-//       owner: context.repo.owner,
-//       repo: context.repo.repo,
-//       pull_number: prNumber,
-//     });
+  const issueBody = response.data.body;
+  console.log(response)
+  console.log(issueBody);
 
-//     const allChangedFiles = fileData.map((file) => file.filename);
+  //   const prNumber = process.env.PR_NUMBER;
 
-//     core.setOutput('files', allChangedFiles.toString());
+  //   if (prNumber === '') {
+  //     core.setFailed(`No PR number was passed. Aborting`);
+  //   }
 
-//     core.setOutput('headRef', head.ref);
-//     core.setOutput('headSHA', head.sha);
-//     core.setOutput('baseRef', base.ref);
-//     core.setOutput('baseSHA', base.sha);
-//     core.setOutput('user', user.login);
-//   } catch (error) {
-//     core.setFailed(`Unable to retrieve info from PR number ${prNumber}.\n\n Error details: ${error}`);
-//     throw error;
-//   }
+  //   try {
+  //     const {
+  //       data: { head, base, user, ...rest },
+  //     } = await github.rest.pulls.get({
+  //       owner: context.repo.owner,
+  //       repo: context.repo.repo,
+  //       pull_number: prNumber,
+  //     });
+
+  //     const { data: fileData } = await github.rest.pulls.listFiles({
+  //       owner: context.repo.owner,
+  //       repo: context.repo.repo,
+  //       pull_number: prNumber,
+  //     });
+
+  //     const allChangedFiles = fileData.map((file) => file.filename);
+
+  //     core.setOutput('files', allChangedFiles.toString());
+
+  //     core.setOutput('headRef', head.ref);
+  //     core.setOutput('headSHA', head.sha);
+  //     core.setOutput('baseRef', base.ref);
+  //     core.setOutput('baseSHA', base.sha);
+  //     core.setOutput('user', user.login);
+  //   } catch (error) {
+  //     core.setFailed(`Unable to retrieve info from PR number ${prNumber}.\n\n Error details: ${error}`);
+  //     throw error;
+  //   }
 };
