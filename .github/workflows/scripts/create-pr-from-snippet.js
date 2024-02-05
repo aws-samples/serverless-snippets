13d6dc2c82efe93bcfd3f18cce29b4fc5829e912
Boyne <dboyne@3c06304f253a.ant.amazon.com>
2024-02-05 11:38:52 +0000

    adding new template

---
+++ .github/workflows/scripts/create-pr-from-snippet.js
@@ -0,0 +1,39 @@
+module.exports = async ({ github, context, core }) => {
+
+  console.log('Trigger', process.env);
+
+//   const prNumber = process.env.PR_NUMBER;
+
+//   if (prNumber === '') {
+//     core.setFailed(`No PR number was passed. Aborting`);
+//   }
+
+//   try {
+//     const {
+//       data: { head, base, user, ...rest },
+//     } = await github.rest.pulls.get({
+//       owner: context.repo.owner,
+//       repo: context.repo.repo,
+//       pull_number: prNumber,
+//     });
+
+//     const { data: fileData } = await github.rest.pulls.listFiles({
+//       owner: context.repo.owner,
+//       repo: context.repo.repo,
+//       pull_number: prNumber,
+//     });
+
+//     const allChangedFiles = fileData.map((file) => file.filename);
+
+//     core.setOutput('files', allChangedFiles.toString());
+
+//     core.setOutput('headRef', head.ref);
+//     core.setOutput('headSHA', head.sha);
+//     core.setOutput('baseRef', base.ref);
+//     core.setOutput('baseSHA', base.sha);
+//     core.setOutput('user', user.login);
+//   } catch (error) {
+//     core.setFailed(`Unable to retrieve info from PR number ${prNumber}.\n\n Error details: ${error}`);
+//     throw error;
+//   }
+};
