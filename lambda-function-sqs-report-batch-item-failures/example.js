a1b6874e28636b5da07360f7f46ee65049e64dbf
Katherine Vishnevskaya <kathvish@amazon.com>
2024-02-08 10:09:53 +1100

    Fixed issues with the javascript snippet

--- lambda-function-sqs-report-batch-item-failures/example.js
+++ lambda-function-sqs-report-batch-item-failures/example.js
@@ -1,24 +1,20 @@
-exports.handler = async (event, context) => {
-
+export const handler = async (event, context) => {
     const batchItemFailures = [];
 
     for (const record of event.Records) {
         try {
             await processMessageAsync(record, context);
         } catch (error) {
-            batchItemFailures.push({ ItemIdentifier: record.messageId });
+            batchItemFailures.push({ itemIdentifier: record.messageId });
         }
     }
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ batchItemFailures }),
-    };
+    return { batchItemFailures };
 };
 
 async function processMessageAsync(record, context) {
-    if (!record.body) {
-        throw new Error('No Body in SQS Message.');
+    if (record.body && record.body.includes("error")) {
+        throw new Error("There is an error in the SQS Message.");
     }
-    context.log(`Processed message ${record.body}`);
+    console.log(`Processed message: ${record.body}`);
 }
