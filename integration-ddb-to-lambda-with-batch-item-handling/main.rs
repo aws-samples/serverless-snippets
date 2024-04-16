b5ce15e7fd14dafeb1ca387d9cca74633eada5d3
Phil Callister <pcallist@amazon.com>
2024-04-15 21:44:34 -0500

    Update to match other Rust snippet examples

--- integration-ddb-to-lambda-with-batch-item-handling/main.rs
+++ integration-ddb-to-lambda-with-batch-item-handling/main.rs
@@ -1,40 +1,73 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-use aws_lambda_events::event::dynamodb::{Event, EventRecord};
+use aws_lambda_events::{
+    event::dynamodb::{Event, EventRecord, StreamRecord},
+    streams::{DynamoDbBatchItemFailure, DynamoDbEventResponse},
+};
 use lambda_runtime::{run, service_fn, Error, LambdaEvent};
-use serde_json::{json, Value};
 
-/// Process your record
-fn process_record(record: &EventRecord) -> Result<(), ()> {
-    // <--- Your record processing logic goes here and returns a Result --->
-    match true {
-        true => Ok(()),
-        _ => Err(()),
-    }
+/// Process the stream record
+fn process_record(record: &EventRecord) -> Result<(), Error> {
+    let stream_record: &StreamRecord = &record.change;
+
+    // process your stream record here...
+    tracing::info!("Data: {:?}", stream_record);
+
+    Ok(())
 }
 
 /// Main Lambda handler here...
-async fn handler(event: LambdaEvent<Event>) -> Result<Value, Value> {
-    let mut cur_record_sequence_number: String = String::from("");
-    let records = &event.payload.records;
-    let result = records.iter().try_for_each(|record| {
-        // Process your record here...
-        if let Some(seq) = &record.change.sequence_number {
-            cur_record_sequence_number = seq.clone();
-            process_record(record)
+async fn function_handler(event: LambdaEvent<Event>) -> Result<DynamoDbEventResponse, Error> {
+    let mut response = DynamoDbEventResponse {
+        batch_item_failures: vec![],
+    };
 
-        // No Sequence number in record
-        } else {
-            Err(())
-        }
-    });
-    if result.is_err() {
-        return Ok(json!({"batchItemFailures": [{"itemIdentifier": cur_record_sequence_number}]}));
+    let records = &event.payload.records;
+
+    if records.is_empty() {
+        tracing::info!("No records found. Exiting.");
+        return Ok(response);
     }
-    Ok(json!({"batchItemFailures": []}))
+
+    for record in records {
+        tracing::info!("EventId: {}", record.event_id);
+
+        // Couldn't find a sequence number
+        if record.change.sequence_number.is_none() {
+            response.batch_item_failures.push(DynamoDbBatchItemFailure {
+                item_identifier: Some("".to_string()),
+            });
+            return Ok(response);
+        }
+
+        // Process your record here...
+        if process_record(record).is_err() {
+            response.batch_item_failures.push(DynamoDbBatchItemFailure {
+                item_identifier: record.change.sequence_number.clone(),
+            });
+            /* Since we are working with streams, we can return the failed item immediately.
+            Lambda will immediately begin to retry processing from this failed item onwards. */
+            return Ok(response);
+        }
+    }
+
+    tracing::info!(
+        "Successfully processed {} records",
+        event.payload.records.len()
+    );
+
+    Ok(response)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    run(service_fn(handler)).await
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        // disable printing the name of the module in every log line.
+        .with_target(false)
+        // disabling time is handy because CloudWatch will add the ingestion time.
+        .without_time()
+        .init();
+
+    run(service_fn(function_handler)).await
 }
