a932fa7531309a4bee84a4e0c96f3303a1ba40e9
Theophilus Ajayi <tolutheo@gmail.com>
2024-04-18 12:46:46 +0000

    fixed export handler compatibility with ES6 module.

--- integration-sns-to-lambda/example.js
+++ integration-sns-to-lambda/example.js
@@ -1,6 +1,6 @@
 ﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-exports.handler = async (event, context) => {
+export const handler = async (event, context) => {
   for (const record of event.Records) {
     await processMessageAsync(record);
   }
