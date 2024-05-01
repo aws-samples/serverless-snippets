b930378af6cc0d112aee04f12bf683877be9ee36
gowthamshankar99 <gowthamshankar0914@gmail.com>
2024-05-01 11:30:05 -0400

    Update example.js

--- integration-ddb-to-lambda-with-batch-item-handling/example.js
+++ integration-ddb-to-lambda-with-batch-item-handling/example.js
@@ -1,17 +1,18 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-function handler(event, context) {
-    const records = event.Records;
-    let curRecordSequenceNumber = "";
+export const handler = async (event) => {
+  const records = event.Records;
+  let curRecordSequenceNumber = "";
 
-    for (const record of records) {
-        try {
-            // Process your record
-            curRecordSequenceNumber = record.dynamodb.SequenceNumber;
-        } catch (e) {
-            // Return failed record's sequence number
-            return { batchItemFailures: [{ itemIdentifier: curRecordSequenceNumber }] };
-        }
+  for (const record of records) {
+    try {
+      // Process your record
+      curRecordSequenceNumber = record.dynamodb.SequenceNumber;
+    } catch (e) {
+      // Return failed record's sequence number
+      return { batchItemFailures: [{ itemIdentifier: curRecordSequenceNumber }] };
     }
-    return { batchItemFailures: [] };
-}
+  }
+
+  return { batchItemFailures: [] };
+};
