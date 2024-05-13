e866e1259d7219fbde6bf49eeaa02e2a120ee8af
Bhuvan Annamreddi <140741942+baannamr-aws@users.noreply.github.com>
2024-05-13 18:33:03 -0500

    Code snippet for Lambda-DynamoDB integration

---
+++ integration-ddb-to-lambda/example.php
@@ -0,0 +1,48 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+<?php
+
+# using bref/bref and bref/logger for simplicity
+
+use Bref\Context\Context;
+use Bref\Event\DynamoDb\DynamoDbEvent;
+use Bref\Event\DynamoDb\DynamoDbHandler;
+use Bref\Logger\StderrLogger;
+
+require __DIR__ . '/vendor/autoload.php';
+
+class Handler extends DynamoDbHandler
+{
+    private StderrLogger $logger;
+
+    public function __construct(StderrLogger $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * @throws JsonException
+     * @throws \Bref\Event\InvalidLambdaEvent
+     */
+    public function handleDynamoDb(DynamoDbEvent $event, Context $context): void
+    {
+        $this->logger->info("Processing DynamoDb table items");
+        $records = $event->getRecords();
+
+        foreach ($records as $record) {
+            $keys = $record->getKeys();
+            $old = $record->getOldImage();
+            $new = $record->getNewImage();
+
+            // TODO: Do interesting work based on the new data
+
+            // Any exception thrown will be logged and the invocation will be marked as failed
+        }
+
+        $totalRecords = count($records);
+        $this->logger->info("Successfully processed $totalRecords items");
+    }
+}
+
+$logger = new StderrLogger();
+return new Handler($logger);
