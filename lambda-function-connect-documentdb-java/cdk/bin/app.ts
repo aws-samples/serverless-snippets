4cf880fa23ad229c627c0b9b7afdd4ff87067c6c
Markus Ziller <web+github@markusziller.de>
2024-06-26 11:05:52 +0200

    Moves stack.ts to cdk/bin/ to escape .gitignore rule in parent directory

--- lambda-function-connect-documentdb-java/cdk/bin/app.ts
+++ lambda-function-connect-documentdb-java/cdk/bin/app.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { DocumentDbLambdaSampleStack } from '../lib/stack';
+import { DocumentDbLambdaSampleStack } from './stack';
 
 const app = new cdk.App();
 new DocumentDbLambdaSampleStack(app, 'DocumentDbLambdaSampleStack');
