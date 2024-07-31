2624fd4ba5acf906dc4580e0c2f01babb6e8726e
Thiago Pádua <tgpadua@gmail.com>
2024-07-31 16:09:06 -0700

    Create Example.java

---
+++ integration-msk-to-lambda/Example.java
@@ -0,0 +1,28 @@
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.KafkaEvent;
+import com.amazonaws.services.lambda.runtime.events.KafkaEvent.KafkaEventRecord;
+
+import java.util.Base64;
+import java.util.Map;
+
+public class Example implements RequestHandler<KafkaEvent, Void> {
+
+    @Override
+    public Void handleRequest(KafkaEvent event, Context context) {
+        for (Map.Entry<String, java.util.List<KafkaEventRecord>> entry : event.getRecords().entrySet()) {
+            String key = entry.getKey();
+            System.out.println("Key: " + key);
+
+            for (KafkaEventRecord record : entry.getValue()) {
+                System.out.println("Record: " + record);
+
+                byte[] value = Base64.getDecoder().decode(record.getValue());
+                String message = new String(value);
+                System.out.println("Message: " + message);
+            }
+        }
+
+        return null;
+    }
+}
