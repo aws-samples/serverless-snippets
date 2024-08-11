06af48e56a9d34976df2cf361e1b79f0ec58a624
Jan Richter <falnyr@gmail.com>
2024-08-11 16:09:10 +1200

    feat: processing kafka events in PHP lambda

---
+++ integration-msk-to-lambda/example.php
@@ -0,0 +1,47 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+<?php
+
+# using bref/bref and bref/logger for simplicity
+
+use Bref\Context\Context;
+use Bref\Event\Kafka\KafkaEvent;
+use Bref\Event\Handler as StdHandler;
+use Bref\Logger\StderrLogger;
+
+require __DIR__ . '/vendor/autoload.php';
+
+class Handler implements StdHandler
+{
+    private StderrLogger $logger;
+    public function __construct(StderrLogger $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * @throws JsonException
+     * @throws \Bref\Event\InvalidLambdaEvent
+     */
+    public function handle(mixed $event, Context $context): void
+    {
+        $kafkaEvent = new KafkaEvent($event);
+        $this->logger->info("Processing records");
+        $records = $kafkaEvent->getRecords();
+
+        foreach ($records as $record) {
+            try {
+                $data = $record->getValue();
+                $this->logger->info(json_encode($data));
+                // TODO: Do interesting work based on the new data
+            } catch (Exception $e) {
+                $this->logger->error($e->getMessage());
+            }
+        }
+        $totalRecords = count($records);
+        $this->logger->info("Successfully processed $totalRecords records");
+    }
+}
+
+$logger = new StderrLogger();
+return new Handler($logger);
