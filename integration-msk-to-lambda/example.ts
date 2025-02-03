706a1de3025e7399036292b6ddde8259a0cb382c
Lavanya0513 <145364950+Lavanya0513@users.noreply.github.com>
2025-02-03 13:42:06 -0500

    Update indendation

--- integration-msk-to-lambda/example.ts
+++ integration-msk-to-lambda/example.ts
@@ -1,35 +1,33 @@
-  import { MSKEvent, Context, MSKRecord } from "aws-lambda";
-  import { Buffer } from "buffer";
-  import { Logger } from "@aws-lambda-powertools/logger";
-  
-  const logger = new Logger({
-    logLevel: "INFO",
-    serviceName: "msk-handler-sample",
-  });
-  
-  export const handler = async (
-    event: MSKEvent,
-    context: Context
-  ): Promise<void> => {
-    for (const [topic, topicRecords] of Object.entries(event.records)) {
-        logger.info(`Processing key: ${topic}`);
-  
-        // Process each record in the partition
-        for (const record of topicRecords) {
-          try {
-            // Decode the message value from base64
-            const decodedMessage = Buffer.from(record.value, 'base64').toString();
-            
-            logger.info({
-              message: decodedMessage
-            });
-            }  
-    catch (error) {
-      logger.error('Error processing event', { error });
-      throw error;
-    }
-  };
-}}
+import { MSKEvent, Context } from "aws-lambda";
+import { Buffer } from "buffer";
+import { Logger } from "@aws-lambda-powertools/logger";
 
-  
-  
+const logger = new Logger({
+  logLevel: "INFO",
+  serviceName: "msk-handler-sample",
+});
+
+export const handler = async (
+  event: MSKEvent,
+  context: Context
+): Promise<void> => {
+  for (const [topic, topicRecords] of Object.entries(event.records)) {
+    logger.info(`Processing key: ${topic}`);
+
+    // Process each record in the partition
+    for (const record of topicRecords) {
+      try {
+        // Decode the message value from base64
+        const decodedMessage = Buffer.from(record.value, 'base64').toString();
+
+        logger.info({
+          message: decodedMessage
+        });
+      }
+      catch (error) {
+        logger.error('Error processing event', { error });
+        throw error;
+      }
+    };
+  }
+}
