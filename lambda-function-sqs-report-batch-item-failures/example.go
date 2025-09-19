fa83e475a28544d501190d29733e832cdc75ef5c
Julian Wood <julianwood@users.noreply.github.com>
2025-09-19 15:35:32 +0100

    Revert "Fix Golang: lambda-function-sqs-report-batch-item-failures"

--- lambda-function-sqs-report-batch-item-failures/example.go
+++ lambda-function-sqs-report-batch-item-failures/example.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -13,12 +14,8 @@ func handler(ctx context.Context, sqsEvent events.SQSEvent) (map[string]interfac
 	batchItemFailures := []map[string]interface{}{}
 
 	for _, message := range sqsEvent.Records {
-		if len(message.Body) > 0 {
-			// Your message processing condition here
-			fmt.Printf("Successfully processed message: %s\n", message.Body)
-		} else {
-			// Message processing failed
-			fmt.Printf("Failed to process message %s\n", message.MessageId)
+		
+		if /* Your message processing condition here */ {			
 			batchItemFailures = append(batchItemFailures, map[string]interface{}{"itemIdentifier": message.MessageId})
 		}
 	}
